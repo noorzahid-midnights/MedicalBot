92d5ffc21fcd0387bc50ea6975e6faadee973493
Noor Zahid <140115738+noorzahid-midnights@users.noreply.github.com>
2026-06-10 20:25:00 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template, jsonify, request
+from src.helper import download_embeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_classic.chains import create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+from src.prompt import *
+import os
+
+app = Flask(__name__)
+
+load_dotenv()
+PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+os.environ["GOOGLE_API_KEY"] = GOOGLE_API_KEY
+
+embedding = download_embeddings()
+index_name = "medical-bot"
+
+docsearch = PineconeVectorStore.from_existing_index(
+    index_name= index_name,
+    embedding= embedding
+)
+
+retriever= docsearch.as_retriever(search_type= "similarity", 
+                                  search_kwargs={"k":5})
+chatModel = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash-lite",
+    temperature=0,
+    max_retries=1
+)
+prompt= ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("human", "Question: {input}")
+    ]
+)
+question_answer_chain= create_stuff_documents_chain(chatModel, prompt)
+rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+
+
+
+@app.route("/")
+def index():
+    return render_template('chats.html')
+
+@app.route("/get", methods= ["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    input= msg
+    print(input)
+    response= rag_chain.invoke({"input":msg})
+    print("Response: ", response["answer"])
+    return str(response["answer"])
+if __name__ =='__main__':
+    app.run(host= "0.0.0.0", port= 8080, debug= True)
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name = "Medical Chatbot",
+    version = "0.1.0",
+    author = "Noor Ul Ain Zahid",
+    author_email = "noorzahid.midnights@gmail.com",
+    packages =find_packages(),
+    install_requires = []
+)
