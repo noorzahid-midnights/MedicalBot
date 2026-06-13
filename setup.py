efec7eca13c8a6242af2391484c22615c08f4dbf
Noor Zahid <140115738+noorzahid-midnights@users.noreply.github.com>
2026-06-13 20:52:28 +0500

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-setup(
-    name = "Medical Chatbot",
-    version = "0.1.0",
-    author = "Noor Ul Ain Zahid",
-    author_email = "noorzahid.midnights@gmail.com",
-    packages =find_packages(),
-    install_requires = []
-)
